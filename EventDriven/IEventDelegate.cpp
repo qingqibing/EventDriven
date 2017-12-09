866dfe6d5665f2cfd5967741e3f85f3b64275f4f
181847 <753090084@qq.com>
2017-12-09 22:35:18 +0800

    修改EventDelegate的实现，取消接口的概念，纯粹使用模板来实现特定事件的分发。

--- EventDriven/IEventDelegate.cpp
+++ EventDriven/IEventDelegate.cpp
@@ -1,17 +1,1 @@
 #include "IEventDelegate.h"
-
-namespace Event
-{
-
-
-IEventDelegate::IEventDelegate()
-{
-}
-
-
-IEventDelegate::~IEventDelegate()
-{
-}
-
-}// namespace Event
-
--- EventDriven/IEventDelegate.h
+++ EventDriven/IEventDelegate.h
@@ -5,47 +5,51 @@
 namespace Event
 {
 
-class IEventDelegate
+template<typename EVENT_TYPE>
+class EventDelegate
 {
 public:
-	IEventDelegate();
-	~IEventDelegate();
-
-	virtual void invoke(IEvent * event) = 0;
+	virtual void invoke(EVENT_TYPE * event) = 0;
 };
 
+
+// the main target of this class is to wrap the caller,
+// and make it easy to call target function, just like std::bind.
 template<typename RECIEVER_TYPE, typename EVENT_TYPE>
-class EventDelegate
-	:public IEventDelegate
+class EventDelegateWrapper
+	:public EventDelegate<EVENT_TYPE>
 {
+	// for short the function name,
+	// give it another name.
 	typedef void (RECIEVER_TYPE::*CallBackFunction)(EVENT_TYPE *);
 public:
-	EventDelegate(RECIEVER_TYPE * reciever, CallBackFunction callBack);
-	~EventDelegate();
+	EventDelegateWrapper(RECIEVER_TYPE * reciever, CallBackFunction callBack);
+	~EventDelegateWrapper();
 
 private:
 	RECIEVER_TYPE * _reciever;
+	// the _callBack is a member function pointer of the _reciever.
 	CallBackFunction _callBack;
 	
-	virtual void invoke(IEvent * event);
+	virtual void invoke(EVENT_TYPE * e) override;
 	
 };
 
 template<typename RECIEVER_TYPE, typename EVENT_TYPE>
-inline EventDelegate<RECIEVER_TYPE, EVENT_TYPE>::EventDelegate(RECIEVER_TYPE * reciever, CallBackFunction callBack)
+inline EventDelegateWrapper<RECIEVER_TYPE, EVENT_TYPE>::EventDelegateWrapper(RECIEVER_TYPE * reciever, CallBackFunction callBack)
 	:_reciever(reciever), _callBack(callBack)
 {
 }
 
 template<typename RECIEVER_TYPE, typename EVENT_TYPE>
-inline EventDelegate<RECIEVER_TYPE, EVENT_TYPE>::~EventDelegate()
+inline EventDelegateWrapper<RECIEVER_TYPE, EVENT_TYPE>::~EventDelegateWrapper()
 {
 }
 
 template<typename RECIEVER_TYPE, typename EVENT_TYPE>
-inline void EventDelegate<RECIEVER_TYPE, EVENT_TYPE>::invoke(IEvent * e)
+inline void EventDelegateWrapper<RECIEVER_TYPE, EVENT_TYPE>::invoke(EVENT_TYPE * e)
 {
-	(_reciever->*_callBack)(reinterpret_cast<EVENT_TYPE*>(e));
+	(_reciever->*_callBack)(e);
 }
 
 }// namespace Event
