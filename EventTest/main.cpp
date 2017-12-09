7f4c25ec0bed4def63c2a4e9014404529433b7d6
181847 <753090084@qq.com>
2017-12-09 19:51:12 +0800

    更新测试中关于TypeTools、IDGenerator的相关名称。增加对IEvent、IEventDelegate的简单。试

--- EventTest/main.cpp
+++ EventTest/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <MyTools/UnitTestModules.h>
-#include "../EventDriven/TypeIDGenerator.h"
+#include "../EventDriven/TypeTools.h"
+#include <TypeTools.h>
+#include <IEvent.h>
+#include <IEventDelegate.h>
+#include "TryEventDelegate.h"
 
 
 DECLARE_TEST_UNITS;
@@ -23,14 +27,36 @@ void AddTestUnit()
 {
 	TEST_UNIT_START("test TypeIDGenerator")
 		int error = 0;
-		auto int_0 = TypeIDGenerator<int>::newID<int>();
-		auto int_1 = TypeIDGenerator<int>::newID<char>();
-		auto int_2 = TypeIDGenerator<int>::newID<std::vector<int>>();
+		auto int_0 = TypeTool::IDGenerator<int>::newID<int>();
+		auto int_1 = TypeTool::IDGenerator<int>::newID<char>();
+		auto int_2 = TypeTool::IDGenerator<int>::newID<std::vector<int>>();
 
 
-		error += NOT_EQ(int_0, TypeIDGenerator<int>::newID<int>());
-		error += NOT_EQ(int_1, TypeIDGenerator<int>::newID<char>());
-		error += NOT_EQ(int_2, TypeIDGenerator<int>::newID<std::vector<int>>());
+		error += NOT_EQ(int_0, TypeTool::IDGenerator<int>::newID<int>());
+		error += NOT_EQ(int_1, TypeTool::IDGenerator<int>::newID<char>());
+		error += NOT_EQ(int_2, TypeTool::IDGenerator<int>::newID<std::vector<int>>());
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("try eventDelegate")
+		int error = 0;
+		static std::vector<Event::IEventDelegate *> delegations;
+
+		TestListener tListenser;
+		tListenser.registerDelegate(delegations);
+
+		TestEvent tEvent;
+		tEvent.x = 22;
+		tEvent.y = 33;
+
+		for (auto & dlgate : delegations)
+		{
+			dlgate->invoke(&tEvent);
+		}
+
+		error += NOT_EQ(22, tListenser.recvX);
+		error += NOT_EQ(33, tListenser.recvY);
+
 		return error == 0;
 	TEST_UNIT_END;
 }
