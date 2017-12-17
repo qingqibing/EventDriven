db50311e2c251ada783b5ce1a66eaeea377d63b9
181847 <753090084@qq.com>
2017-12-17 17:49:02 +0800

    删除  IEventListener，因为我们的EventHandler注册Listener方式的修改，不再需要那个类似接口的class了。

--- EventDriven/EventListener.h
+++ EventDriven/EventListener.h
@@ -4,28 +4,18 @@
 
 namespace Event
 {
-// next two class {IEventListener/EventListener} have
-// no significant function,
-// the main goal of them is to simplify the process
-// add delegate to the EventHandler.
 
-// IEventListener is not a interface,
-// it has no virtual function,
-// but it functionality is same as the interface,
-// it is mainly used in the EventHandler.
-// Throught the function{getDelegate()},
-// the EventHandler can get the delegate from it.
-// And the template arg DERIVED_LISTENER is the 
-// final Class which implement the callBack function,
-// all the callBack function should have the same name
-// "ListenEvent", but can have different parameter type( only 1 parameter)
-// for different EventType.
-template<typename DERIVED_LISTENER>
-class IEventListener
+// The argument of the template:
+// DERIVED_LISTENER: the class who implement the ListenEvent()function.
+// LISTENED_EVENTS: All the events that the listener will listen,
+//					basically it is just store the Event types, and make the eventHandler easy to find
+//					which type of event the listener care about.
+// be aware that there should be no virtual function of the DERIVED_LISTENER,
+// or the reinterpret_cast will not work.
+template<typename DERIVED_LISTENER, typename...LISTENED_EVENTS>
+class EventListener
 {
 public:
-	typedef DERIVED_LISTENER DerviedListener;
-
 	// get the delegate, 
 	// the delegate is on the heap,
 	// but the dispatcher will delete it,
@@ -40,26 +30,26 @@ public:
 		getDelegateID();
 };
 
-template<typename DERIVED_LISTENER>
+template<typename DERIVED_LISTENER, typename ...LISTENED_EVENTS>
 template<typename EVENT>
-inline Event::EventDelegate<EVENT>* IEventListener<DERIVED_LISTENER>::getDelegate()
+inline Event::EventDelegate<EVENT>* EventListener<DERIVED_LISTENER, LISTENED_EVENTS...>::getDelegate()
 {
-	void(DerviedListener::*callBack)(EVENT *);
-	callBack = &DerviedListener::ListenEvent;
+	void(DERIVED_LISTENER::*callBack)(EVENT *);
+	callBack = &DERIVED_LISTENER::ListenEvent;
 	return new Event::EventDelegateWrapper<DERIVED_LISTENER, EVENT>
 		(reinterpret_cast<DERIVED_LISTENER*>(this), callBack);
 }
 
-template<typename DERIVED_LISTENER>
+template<typename DERIVED_LISTENER, typename ...LISTENED_EVENTS>
 template<typename EVENT>
-inline Event::DelegateID IEventListener<DERIVED_LISTENER>::getDelegateID()
+inline Event::DelegateID EventListener<DERIVED_LISTENER, LISTENED_EVENTS...>::getDelegateID()
 {
 	// the same delegate of the same listener 
 	// will use the same receiver type and event type.
 	// only the pointer to reciever is different.
 	// so here use a static var to store the typeID,
 	// but the pointer to reciever will be set with differ object.
-	static DelegateID returnedID = {
+	static Event::DelegateID returnedID = {
 		Event::RecvIDGenerator::newID<DERIVED_LISTENER>(),
 		0,	// init as nullptr
 		Event::EventIDGenerator::newID<EVENT>() };
@@ -68,17 +58,5 @@ inline Event::DelegateID IEventListener<DERIVED_LISTENER>::getDelegateID()
 	returnedID.pReciever = reinterpret_cast<Event::ID>(this);
 	return returnedID;
 }
-
-// The EventListener is barely a template storage place,
-// it has no function, all it goal is to store the desired Event Type,
-// and pass the class(DERIVED_LISTENER) which implement the callBack
-// function forward to the IEventListener. 
-template<typename DERIVED_LISTENER, typename...LISTENED_EVENTS>
-class EventListener
-	:public IEventListener<DERIVED_LISTENER>
-{
-
-};
-
 
 }// namespace Event
